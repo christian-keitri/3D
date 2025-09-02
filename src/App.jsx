cafc97c25c312828bb7af8c0502de45f51f66b5a
BOB <christian.salapate@keitri.com>
2025-09-02 10:21:36 +0800

    must work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { useEffect } from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
@@ -11,40 +10,33 @@ import ProjectsSection from "./components/ProjectsSection"
 import ContactSection from "./components/ContactSection"
 import Footer from "./components/Footer"
 import ProgressBar from "./components/ProgressBar"
-import ThankYou from "./components/ThankYou" // make sure you import this
+
 
 export default function App() {
+
   useEffect(() => {
+    //Register Scroll Trigger plugin
     gsap.registerPlugin(ScrollTrigger)
+
+    //REfresh Scroll Trigger when the page is fully loaded
     ScrollTrigger.refresh()
+
+    //Clean up ScrollTrigger on component unmount
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
     }
   }, [])
-
   return (
-    <Router>
-      <Routes>
-        {/* Home Page */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <HeroSection />
-              <CustomCursor />
-              <AboutSection />
-              <ProjectsSection />
-              <ContactSection />
-              <Footer />
-              <ProgressBar />
-            </>
-          }
-        />
+    <>
+      <Header />
+      <HeroSection />
+      <CustomCursor />
+      <AboutSection />
+      <ProjectsSection />
+      <ContactSection />
+      <Footer />
+      <ProgressBar />
 
-        {/* Thank You Page */}
-        <Route path="/thank-you" element={<ThankYou />} />
-      </Routes>
-    </Router>
+    </>
   )
 }
